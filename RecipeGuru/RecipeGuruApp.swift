4cdaf04e30884f29f7c0dcdb5b86c326daf97001
JKnight3517 <knightjknight@gmail.com>
2023-08-29 11:54:27 -0700

    Initial Commit

---
+++ RecipeGuru/RecipeGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeGuruApp.swift
+//  RecipeGuru
+//
+//  Created by Justin Knight on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
